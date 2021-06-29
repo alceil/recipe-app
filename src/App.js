91f10bdb8499099d6d64d6eeec90027a10db4d42
alceil <ashishshajisrampickal@gmail.com>
2021-06-29 22:27:52 +0530

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState,useRef} from 'react'
+
 
 function App() {
+  const inputRef = useRef(null)
+
+const search=()=>{
+  searchForRecipe(inputRef.current.value)
+  inputRef.current.value=''
+}
+
+const searchForRecipe=(query)=>
+{
+  setLoading(true)
+  const url =`https://api.edamam.com/search?q=${query}&app_id=2ef4d7db&app_key=25bbb9dec73ce7e1fe43163d963fe3b4`;
+  fetch(url,{node:"nno-cors"})
+  .then(response=>response.json()).then(res=>{
+    setIngredientsList(res.hits)
+    setLoading(false)
+    console.log(res)
+  })
+  .catch(err=>
+    {
+      console.log("error",err)
+      setLoading(false)
+    });
+}
+const [ingredientsList,setIngredientsList]=useState([]);
+const [loading,setLoading]=useState(false);
+  useEffect(()=>
+  {
+    searchForRecipe('chicken')
+  },[]);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="inputWrapper">
+          <input ref={inputRef} placeholder ="Search for recipe" type="text" />
+          <button onClick={search}>Search</button>
+        </div>
+
+        <div>
+          {loading&& <p>Loading...</p>  }
+        </div>
+        <div className="Wrapper">
+          {ingredientsList.map(({recipe})=>{
+            const {label,image,ingredientLines}=recipe
+            return(
+              <div key ={label} className="Ingredient">
+                <span>
+                  {label}
+                </span>
+<img src={image}  />
+
+<div className="steps">
+{ingredientLines.map((step,index)=>{
+
+return <p key={index}>{step}</p>
+})}
+</div>
+
+              </div>
+            )
+          })}
+
+        </div>
       </header>
     </div>
   );
